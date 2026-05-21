98886ee43a96e31193d7aeaebf09100f936bc7e7
Karishma <karishma.7022@gmail.com>
2026-05-21 19:14:51 +0530

    step 3: manually promisify fs.readFile

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("config.txt","utf-8",callback);
-
+/*fs.readFile("config.txt","utf-8",callback);
 function callback(err,data){
     if(data){
         console.log(data);
@@ -9,4 +8,26 @@ function callback(err,data){
     else{
         console.log(err);
     }
-}
+}*/
+
+
+function readFilePromisified(fileName){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(fileName,"utf-8",(err,data)=>{
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
+        })
+    })
+};
+
+readFilePromisified("config.txt")
+.then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
